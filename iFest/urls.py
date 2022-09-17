fdb00520ec2fbccaeae2922ef202c440ab300fd1
kavan02 <kavanbuch02@gmail.com>
2022-09-17 19:26:52 +0530

    Added previous css and added some routs in urls.py

--- iFest/urls.py
+++ iFest/urls.py
@@ -41,10 +41,25 @@ urlpatterns = [
     path('moreInfo/', views.moreInfo, name='moreInfo'),
     path('team/', views.ifestTeam, name='ifestTeam'),
     path('sponsors/', views.sponsors, name='sponsors'),
+    
+    path('reset_password/',
+         auth_views.PasswordResetView.as_view(template_name="password_reset.html"),
+         name="reset_password"),
+
+    path('reset_password_sent/',
+         auth_views.PasswordResetDoneView.as_view(template_name="password_reset_sent.html"),
+         name="password_reset_done"),
+
+    path('reset/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(template_name="password_reset_form.html"),
+         name="password_reset_confirm"),
+
+    path('reset_password_complete/',
+         auth_views.PasswordResetCompleteView.as_view(template_name="password_reset_done.html"),
+         name="password_reset_complete"),
 
     path('iohunt/', include('iohunt.urls')),
     path('quiz/', include('quiz.urls'))
 ]
-
 
 #urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
